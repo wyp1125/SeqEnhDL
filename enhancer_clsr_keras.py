df6fa4840849332458c11a4a8fbd1d9f9c3248ca
yupeng wang <wyp1125@gmail.com>
2019-07-31 21:13:09 -0500

    update keras

--- enhancer_clsr_keras.py
+++ enhancer_clsr_keras.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
-from tensorflow.keras import layers
-from tensorflow.keras.callbacks import TensorBoard
-from time import time
-
+import keras
+from keras import layers
+from keras.callbacks import ModelCheckpoint
+from keras.models import model_from_json,load_model
 import numpy as np
 import argparse
 parser = argparse.ArgumentParser(description='Process input files and parameters.')
@@ -13,7 +13,8 @@ parser.add_argument('-y2', '--tsty', type=str, required=True, help="testing outc
 parser.add_argument('-r', '--rate', type=float, required=False, help="learning rate")
 parser.add_argument('-c', '--epochs', type=int, required=False, help="number of epochs")
 parser.add_argument('-b', '--batch_size', type=int, required=False, help="batch size")
-parser.add_argument('-l', '--log', type=str, required=False, help="log file")
+parser.add_argument('-m', '--model',type=str, required=False, help="save best model to file")
+parser.add_argument('-l', '--log', type=str, required=False, help="log file for accuracy of best model")
 args = parser.parse_args()
 learning_rate = 0.001
 if args.rate:
@@ -24,11 +25,15 @@ if args.epochs:
 batch_size = 512
 if args.batch_size:
     batch_size=args.batch_size
-use_tb=False
-log_dir=""
+saved_model = ""
+if args.model:
+    saved_model=args.model
+log_file = ""
 if args.log:
-    use_tb=True
-    log_dir=args.log
+    if saved_model=="":
+        print("saved_model must be specified when using this option")
+        quit()
+    log_file=args.log
 
 Xtrn=np.loadtxt(args.trnx)
 Xtrn_len=Xtrn.shape[0]
@@ -43,16 +48,33 @@ Xtst=Xtst.reshape((Xtst_len,200,4))
 
 Ytst=np.loadtxt(args.tsty)
 
-model = tf.keras.Sequential()
+model = keras.Sequential()
 model.add(layers.Bidirectional(layers.LSTM(128)))
+#model.add(layers.Flatten())
+model.add(layers.Dense(128, activation='relu'))
+model.add(layers.Dropout(0.5))
 model.add(layers.Dense(ncls, activation='softmax'))
 model.compile(optimizer=tf.train.AdamOptimizer(learning_rate),
               loss='categorical_crossentropy',
               metrics=['accuracy'])
-if use_tb:
-    tensorboard = TensorBoard(log_dir=log_dir)
-    model.fit(Xtrn, Ytrn, validation_data=(Xtst,Ytst), epochs=training_epochs, batch_size=batch_size, callbacks=[tensorboard])
+if saved_model=="":
+    model.fit(Xtrn, Ytrn, validation_data=(Xtst,Ytst), epochs=training_epochs, verbose=1, batch_size=batch_size)
 else:
-    model.fit(Xtrn, Ytrn, validation_data=(Xtst,Ytst), epochs=training_epochs, batch_size=batch_size)
-
-
+    checkpoint = ModelCheckpoint(filepath=saved_model+'.h5', verbose=1, monitor='val_loss',save_best_only=True,save_weights_only=True)  
+    model.fit(Xtrn, Ytrn, validation_data=(Xtst,Ytst), epochs=training_epochs, verbose=1, callbacks=[checkpoint], batch_size=batch_size)
+    model_json=model.to_json()
+    with open(saved_model+'.json',"w") as of:
+        of.write(model_json)
+        of.close()
+    if log_file!="":
+        with open(log_file,"w") as of:
+           json_file=open(saved_model+'.json','r')
+           loaded_model_json=json_file.read()
+           json_file.close() 
+           loaded_model=model_from_json(loaded_model_json)
+           loaded_model.load_weights(saved_model+'.h5')
+           loaded_model.compile(optimizer=tf.train.AdamOptimizer(learning_rate),
+                                loss='categorical_crossentropy',
+                                metrics=['accuracy'])
+           score=loaded_model.evaluate(Xtst, Ytst, verbose=1)
+           of.write("%s: %.3f\n" % (loaded_model.metrics_names[1], score[1]))
