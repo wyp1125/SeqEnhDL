4f48366485f3ae1a88df372fcf71303199896a3a
Yupeng  Wang <wangy30@biowulf.nih.gov>
2019-10-28 14:41:30 -0400

    clean

--- paral_scan_sel_new_ctl2.pl
+++
@@ -1,7 +0,0 @@
-@a=("gm12878","H1hesc","hepg2","Hmec","Hsmm","Huvec","K562","Nhek","Nhlf");
-for($i=0;$i<9;$i++)
-{
-$cmd="./a.out ../genome_masked_$a[$i] ../segmentation/$a[$i].str_enh.210bp.filtered.fea ../segmentation2/$a[$i].str_enh.210bp.filtered.ctl.fa 0.01 0 3";
-print $cmd,"\n";
-system($cmd);
-}
